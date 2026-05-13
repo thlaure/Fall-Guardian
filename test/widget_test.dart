2cf8704602ef5a76f7d3133ee234ab3ad7ab8547
Thomas Laure <thomas.laure@enovacom.com>
2026-05-13 15:14:17 +0200

    test: update caregiver home widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:caregiver_app/main.dart';
+import 'package:caregiver_app/l10n/app_localizations.dart';
+import 'package:caregiver_app/screens/home_screen.dart';
 
 void main() {
   testWidgets('renders caregiver scaffold home', (tester) async {
-    await tester.pumpWidget(const CaregiverApp());
+    await tester.pumpWidget(
+      MaterialApp(
+        locale: const Locale('en'),
+        localizationsDelegates: const [AppLocalizations.delegate],
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const CaregiverHomeScreen(isLinked: false),
+      ),
+    );
+    await tester.pump();
 
     expect(find.text('Fall Guardian Caregiver'), findsOneWidget);
-    expect(find.text('Caregiver app scaffolded'), findsOneWidget);
-    expect(find.textContaining('separate caregiver client'), findsOneWidget);
+    expect(find.text('Not Linked Yet'), findsOneWidget);
+    expect(find.text('Link with Protected Person'), findsOneWidget);
+    expect(find.text('How it works'), findsOneWidget);
   });
 }
